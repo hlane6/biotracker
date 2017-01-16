b6c4965ba97c1cf3d7a58cc9c201e0d89fef414a
Harry <hlane6@gatech.edu>
2017-01-16 13:49:28 -0500

    Simplifies template component, adds links that can be useful later

--- biotracker/__init__.py
+++ biotracker/__init__.py
@@ -1,5 +1,10 @@
 import flask as fl
 
+""" Consider using these flask extensions:
+    Flask-Uploads - http://pythonhosted.org/Flask-Uploads/#flaskext.uploads.UploadSet.save
+    Flask-RESTful - https://flask-restful.readthedocs.io/en/0.3.5/
+"""
+
 app = fl.Flask(__name__)
 
 import biotracker.views
