1967c2a0c2f0716abc3e5ab0f873dd06a8e3e0a2
Harry <hlane6@gatech.edu>
2017-02-01 14:53:28 -0500

    Gets webpack working with jsx files

---
+++ app/client/components/Button.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+/**
+ * Simple Button component
+ */
+export default class Button extends React.Component {
+
+    static defaultProps = {
+        handler: () => {},
+        text: '',
+    };
+
+    static propTypes = {
+        handler: React.PropTypes.func,
+        text: React.PropTypes.string,
+    };
+
+    render() {
+        return (
+          <button
+            className="button"
+            onClick={this.props.handler}
+          >
+            {this.props.text}
+          </button>
+        );
+    }
+}
