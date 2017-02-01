5f50870f9d7f3a8bd92ba5076c1b816af176bd5c
Harry <hlane6@gatech.edu>
2017-02-01 15:37:43 -0500

    Tracker conforms to style guide

--- app/client/components/Tracker.jsx
+++ app/client/components/Tracker.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import VideoCanvas from './VideoCanvas'
+import VideoCanvas from './VideoCanvas';
 
 export default class Tracker extends React.Component {
 
@@ -8,45 +8,45 @@ export default class Tracker extends React.Component {
         this.state = {
             paused: true,
             frame: 0,
-        }
+        };
         this.handlePlayPause = this.handlePlayPause.bind(this);
         this.handleSeek = this.handleSeek.bind(this);
         this.handleDraw = this.handleDraw.bind(this);
         this.handleClick = this.handleClick.bind(this);
     }
 
-    render() {
-        return (
-            <div>
-                <VideoCanvas
-                        paused={ this.state.paused }
-                        frame={ this.state.frame }
-                        src={ '/video' }
-                        playPauseCallback={ this.handlePlayPause }
-                        seekCallback={ this.handleSeek }
-                        drawCallback={ this.handleDraw }
-                        onClick={ this.handleClick }
-                />
-            </div>
-        )
-    }
-
     handlePlayPause(paused) {
-        this.setState({ paused: paused });
+        this.setState({ paused });
     }
 
     handleSeek(frame) {
-        this.setState({ frame: frame });
+        this.setState({ frame });
     }
 
     handleDraw() {
-        this.setState((prevState, props) => { return {
-            frame: prevState.frame + 1
-        }});
+        this.setState(prevState => ({ frame: prevState.frame + 1 }));
     }
 
+    /* eslint-disable */
+    // FIXME: implement clicking and remove eslint
     handleClick(event) {
         console.log(event);
     }
+    /* eslint-enable */
 
+    render() {
+        return (
+          <div>
+            <VideoCanvas
+              paused={this.state.paused}
+              frame={this.state.frame}
+              src={'/video'}
+              playPauseCallback={this.handlePlayPause}
+              seekCallback={this.handleSeek}
+              drawCallback={this.handleDraw}
+              onClick={this.handleClick}
+            />
+          </div>
+        );
+    }
 }
