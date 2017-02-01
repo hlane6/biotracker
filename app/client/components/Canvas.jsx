e4e52586ae094c33d6b789b8fd06b780da12b237
Harry <hlane6@gatech.edu>
2017-02-01 15:16:08 -0500

    Canvas conforms to style guide

--- app/client/components/Canvas.jsx
+++ app/client/components/Canvas.jsx
@@ -9,19 +9,19 @@ export default class Canvas extends React.Component {
 
     // Defaults to no callback and 30 FPS
     static defaultProps = {
-        draw        : function() {},
-        onClick     : function() {},
-        step        : 33.367,
-        width       : 600,
-        height      : 600
+        draw: () => {},
+        onClick: () => {},
+        step: 33.367,
+        width: 600,
+        height: 600,
     };
 
     static propTypes = {
-        draw        : React.PropTypes.func,
-        onClick     : React.PropTypes.func,
-        step        : React.PropTypes.number,
-        width       : React.PropTypes.number,
-        height      : React.PropTypes.number
+        draw: React.PropTypes.func,
+        onClick: React.PropTypes.func,
+        step: React.PropTypes.number,
+        width: React.PropTypes.number,
+        height: React.PropTypes.number,
     };
 
     constructor(props) {
@@ -30,8 +30,8 @@ export default class Canvas extends React.Component {
         this.state = { progress: 0 };
 
         this.style = {
-            width: props.width + "px",
-            height: props.height + "px",
+            width: `${props.width}px`,
+            height: `${props.height}px`,
         };
     }
 
@@ -39,48 +39,45 @@ export default class Canvas extends React.Component {
         this.forceUpdate();
     }
 
-    render() {
-        const { draw, ...other } = this.props;
-        requestAnimationFrame(this.requestAnimationFrameCallback);
-
-        return (
-            <canvas style={ this.style }
-                    className="canvas"
-                    ref='canvas'
-                    { ...other }>
-                { this.props.children }
-            </canvas>
-        );
-    }
-
     requestAnimationFrameCallback(time) {
         if (this.previousFrameTime !== time) {
-            const { props, refs } = this;
-            const { draw, step } = props;
-            const ctx = (refs && refs.canvas && refs.canvas.getContext('2d'));
+            const ctx = (this.canvas && this.canvas.getContext('2d'));
 
             let delta = 0;
 
-            if (draw && ctx) {
+            if (this.props.draw && ctx) {
                 requestAnimationFrame(this.requestAnimationFrameCallback);
 
                 if (!this.previousFrameTime) {
                     this.previousFrameTime = time;
                 } else {
-                    delta = time - this.previousFrameTime
+                    delta = time - this.previousFrameTime;
                 }
 
                 this.previousFrameTime = time;
             }
 
-            if (this.state.progress + delta > step) {
-                draw({ time: time, delta: delta, ctx: ctx });
+            if (this.state.progress + delta > this.props.step) {
+                this.props.draw({ time, delta, ctx });
             }
 
-            this.setState((prevState, props) => ({
-                progress: (prevState.progress + delta > step) ? 0 : prevState.progress + delta
+            this.setState(prevState => ({
+                progress: (prevState.progress + delta > this.props.step)
+                        ? 0
+                        : prevState.progress + delta,
             }));
         }
     }
 
+    render() {
+        requestAnimationFrame(this.requestAnimationFrameCallback);
+        return (
+          <canvas
+            style={this.style}
+            className="canvas"
+            ref={(input) => { this.canvas = input; }}
+            onClick={this.props.onClick}
+          />
+        );
+    }
 }
