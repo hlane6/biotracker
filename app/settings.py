c2bb2d4e6521a303b1180a2b7a0d6361cf60634e
Alex Matlock <amatlock22@gmail.com>
2017-04-02 15:19:24 -0400

    Added DEFAULT_SETTINGS to file. Created tracker script.

---
+++ app/settings.py
@@ -0,0 +1,10 @@
+DEFAULT_SETTINGS = {
+	'VID_FOLDER': './biotracker/static/video/',
+	'DATA_FOLDER': './biotracker/static/data/',
+	'BKGRND_FOLDER': './biotracker/static/backgrounds/',
+	'HISTORY_LATENCY': 15,
+	'MIN_BLOB_SIZE': 150,
+	'MAX_BLOB_SIZE': 700,
+	'VID_FOLDER_RELATIVE': 'video',
+	'DATA_FOLDER_RELATIVE': 'data'
+}
---
+++ app/trackerApp.py
@@ -0,0 +1,19 @@
+from biotracker.models.targetManager import TargetManager
+from settings import DEFAULT_SETTINGS
+
+import os.path
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Use this app to generate tracklets for a video. NOTE: Make sure input video is located within the video folder. Input video should only be in the form "videoname.mp4".')
+    parser.add_argument('video', help='video of targets in the form "videoname.mp4"')
+    args = parser.parse_args()
+    args.video = DEFAULT_SETTINGS['VID_FOLDER'] + args.video
+    if os.path.isfile(args.video):
+        fname = os.listdir(DEFAULT_SETTINGS['VID_FOLDER'])[0]
+        mgr = TargetManager('{}/{}'.format(DEFAULT_SETTINGS['VID_FOLDER'], fname))
+        mgr.identify_targets()
+        csv_path = os.path.join(DEFAULT_SETTINGS['DATA_FOLDER'], fname.split('.')[0] + '.csv')
+        mgr.write_csv_file(csv_path)
+    else:
+        print("Can't find video.")
