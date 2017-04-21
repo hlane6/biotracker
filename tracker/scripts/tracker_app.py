0d7a291fdced5383dcb9c6f5ba0249fd481a1918
Alex Matlock <amatlock22@gmail.com>
2017-04-21 12:54:31 -0400

    Command line scripts (#72)
    * Finalized scripts
    
    * Updated video codec for linux
    
    * Removed pointless background image check.
    
    * Removed pointless path check.

--- tracker/scripts/tracker_app.py
+++ tracker/scripts/tracker_app.py
@@ -2,27 +2,11 @@
 video file.
 """
 
-import os.path
-import argparse
-
 from ..models.target_manager import TargetManager
 
-
-def tracker():
-    parser = argparse.ArgumentParser(description='Use this app to generate tracklets for a video. NOTE: Make sure input video is located within the video folder. Stores the output csv in the data folder.')
-
-    parser.add_argument('video', help='video of targets in the form "videoname.mp4"')
-    # parser.add_argument('background', required=False, help='Background image used to perform tracking. Providing this will considerable speed up tracking.')
-
-    args = parser.parse_args()
-
-    if os.path.isfile(args.video):
-        manager = TargetManager(args.video)
-
-        manager.identify_targets()
-        manager.associate_targets()
-
-        csv_path = args.video.split('.mp4')[0] + '.csv'
-        manager.write_csv_file(csv_path)
-    else:
-        print("Can't find video: '" + args.video + "'.")
+def tracker(video, background=None):
+    manager = TargetManager(video, background)
+    manager.identify_targets()
+    manager.associate_targets()
+    csv_path = video.split('.mp4')[0] + '.csv'
+    manager.write_csv_file(csv_path)
